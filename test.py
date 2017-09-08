26f9bbad2e5797d674b163674244e261f285f9eb
Mateusz Karabela <m.karabela@PREVAC.EU.local>
2017-09-08 15:27:09 +0200

    developement :D

--- test.py
+++ test.py
@@ -1,27 +1,3 @@
-from lib_mbtcplogger import cTag
-from lib_mbtcplogger import MBTag
-from lib_mbtcplogger import TimestampTag
-from lib_mbtcplogger import MBInterface
-from lib_mbtcplogger import IndexTag
-from lib_mbtcplogger import cConfReader
-import logging
+from pymodbus.client.sync import ModbusTcpClient as ModbusClient
 
-logging.basicConfig(level=logging.DEBUG)
-logging.debug('Program start')
-
-mb = MBInterface()
-
-
-conf = cConfReader('jobconfig.ini')
-conf.Parse()
-
-
-
-for tag in conf.getTagsList():
-    #print(tag)
-    if isinstance(tag, MBTag):
-        tag.attachToMBInterface(mb)
-
-for tag in conf.getTagsList():
-    print(tag.retrieveValue())
-
+client = ModbusClient('10.10.64.217',502)
