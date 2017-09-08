aaee66712d52d51c3027a9b135a2e9a32c3c3a4c
Mateusz Karabela <m.karabela@PREVAC.EU.local>
2017-09-08 15:28:21 +0200

    developement...

--- lib_mbtcplogger.py
+++ lib_mbtcplogger.py
@@ -2,6 +2,7 @@ import logging
 import configparser
 from pymodbus.client.sync import ModbusTcpClient as ModbusClient
 import datetime
+import time
 
 class cTag(object):
     def __init__(self, tag_name):
@@ -31,10 +32,10 @@ class MBTag(cTag):
         config.read(configfile)
         self.tag_desc = config[self.tag_name]['tag_desc']
         self.ip = config[self.tag_name]['slave_ip']
-        self.port = config[self.tag_name]['slave_port']
-        self.unit = config[self.tag_name]['slave_unit']
+        self.port = int(config[self.tag_name]['slave_port'])
+        self.unit = int(config[self.tag_name]['slave_unit'])
         self.method = config[self.tag_name]['method']
-        self.offset = config[self.tag_name]['address']
+        self.offset = int(config[self.tag_name]['address'])
     def attachToMBInterface(self, mb_interface):
         logging.debug("MBTag attachToMBInterface")
         self.mb_interface = mb_interface
@@ -49,7 +50,7 @@ class TimestampTag(cTag):
     def retrieveValue(self):
         logging.debug("TimestampTag retrieveValue")
         now = datetime.datetime.now()
-        return now.strftime("%Y-%m-%d %H:%M:%S")
+        return now.strftime("%Y-%m-%d %H:%M:%S.%f")
     def selfConfig(self, configfile):
         logging.debug("TimestampTag selfConfig")
         pass
@@ -84,7 +85,7 @@ class MBInterface(ModbusClient):
         got_it = False
         client_to_use = None
         for cl in self.clientslist:
-            if cl.host == ip:
+            if (cl.host == ip) and (cl.port == port):
                 # uzyj tego obiektu klienta
                 logging.debug("MBInterface Query - host already known")
                 got_it = True
@@ -93,9 +94,21 @@ class MBInterface(ModbusClient):
             logging.debug("MBInterface Query - host used for first time")
             client_to_use = ModbusClient(ip,port)
             self.clientslist.append(client_to_use)
+        
+       
         logging.debug("MBInterface Query - assembling query: {0}:{1} unit={2} type={3} address={4}".format(client_to_use.host, client_to_use.port, unit, querytype, address))
-        logging.debug("TODO: actual query request!!!!!!")
-        return 234.5
+        
+        returnvalue = self.ReadHoldingRegister(client_to_use, unit, address)
+        
+        return returnvalue
+     
+    def ReadHoldingRegister(self, clientobj, unit_addr, reg_addr):
+        logging.debug("MBInterface ReadHoldingRegister")
+        #print("client.host = {0}, port = {1}".format(clientobj.host, clientobj.port))
+        if clientobj.socket == None:
+            clientobj.connect()
+        ret_val = clientobj.read_holding_registers(reg_addr, 1, unit = unit_addr)
+        return ret_val.registers[0]
 
 
 class cConfReader(object):
@@ -135,7 +148,45 @@ class cConfReader(object):
             self.tagslist.append(tmp)
             
         # read some common parameters:
-        self.poll_interval_ms = self.config.defaults()['poll_interval_ms']
-        self.delimiter_char = self.config.defaults()['delimiter_char]
+        self.poll_interval_ms = int(self.config.defaults()['poll_interval_ms'])
+        self.delimiter_char = self.config.defaults()['delimiter_char']
     def getTagsList(self):
         return self.tagslist
+
+class Logger(object):
+    def __init__(self, interval, tags_list, delimiter):
+        self.interval = interval
+        self.tags_list = tags_list
+        self.output_file = None
+        self.delimiter = delimiter
+    def ExecutePoll(self):
+        result_list = []
+        for tag in self.tags_list:
+            result_list.append(tag.retrieveValue())
+        return result_list
+    def Loop(self):
+        loop_time = time.time()
+        error = 0
+        delay_s = 0.001*self.interval
+        while True:
+            starttime = time.time()
+            res = self.ExecutePoll()
+            # --- actual logging:
+            self.LogResultToFile(res)
+            self.PrintResult(res)
+            # ---
+            endtime = time.time()
+            remainingtime = delay_s - (endtime - starttime)
+            time.sleep(remainingtime - error)
+            # time drift compensation
+            error = error -delay_s - loop_time + time.time()
+            loop_time = time.time()
+            
+    def LogResultToFile(self, result_list):
+        pass
+       
+    def PrintResult(self, result_list):
+        for result in result_list:
+            print('{0}{1}'.format(result, self.delimiter), end='')
+        print('')
+        
--- mbtcplogger.py
+++ mbtcplogger.py
@@ -1,12 +1,19 @@
 from pymodbus.client.sync import ModbusTcpClient as ModbusClient
-import logging
 import configparser
 import sys
+from lib_mbtcplogger import cTag
+from lib_mbtcplogger import MBTag
+from lib_mbtcplogger import TimestampTag
+from lib_mbtcplogger import MBInterface
+from lib_mbtcplogger import IndexTag
 from lib_mbtcplogger import cConfReader
+from lib_mbtcplogger import Logger
+import logging
+import time
 
 logging.basicConfig()
 log = logging.getLogger()
-log.setLevel(logging.DEBUG)
+log.setLevel(logging.INFO)
 
 if len(sys.argv)<2:
     log.debug("no config file!")
@@ -14,5 +21,15 @@ if len(sys.argv)<2:
 else:
     log.debug("reading ini file: {0}".format(sys.argv[1]))
 
-confreader = cConfReader(sys.argv[1])
-confreader.Parse()
+conf = cConfReader(sys.argv[1])
+conf.Parse()
+
+mb = MBInterface()
+for tag in conf.getTagsList():
+    if isinstance(tag, MBTag):
+        tag.attachToMBInterface(mb)
+
+logger = Logger(conf.poll_interval_ms, conf.getTagsList(), conf.delimiter_char)
+#print(logger.ExecutePoll())
+logger.Loop()
+    
